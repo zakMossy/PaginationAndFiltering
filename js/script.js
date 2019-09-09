7156e14e3a3ce7ba4e61ee7757ab799a0acf4a18
Zak Mosbacher <zakmosbacher@gmail.com>
2019-09-08 22:40:59 -0700

    Small Bug Fix
    Accidental mistake with the event.'click'

--- js/script.js
+++ js/script.js
@@ -10,7 +10,8 @@ var students = document.getElementsByClassName("student-item cf");    // selects
       students[i].style.display = "none";                            // hides all student list elements
     }
     var startIndex = (page * 10) - 10;                              // gets the starting index of the students on the specified page
-    var endIndex = page * 10 - 1;                                  // gets the final index of the students on the specified page
+    var endIndex = page * 10 - 1;                             // gets the final index of the students on the specified page
+
     for(i=startIndex;i<=endIndex;i+=1){
       students[i].style.display = "block";                         // displays the students on the specified page
     }
@@ -35,7 +36,7 @@ var students = document.getElementsByClassName("student-item cf");    // selects
     var a = document.querySelectorAll("a[href='#']");            // selects the anchors of all li tags b/c you cant use the same variable inside of the function because of its scope
     a[0].className = "active";                                 // gives the class of active to the first anchor tag
     for(i=0;i<listLength;i+=1){
-      a[i].addEventListener(event.'click', event=>{
+      a[i].addEventListener('click', event=>{
         var active = document.getElementsByClassName("active");
         active[0].classList.remove("active");                                        // gets rid of all exisiting active tags on the anchors
         event.target.className = "active";  Math.ceil()              // gives the class of active to the anchor that was clicked
